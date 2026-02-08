0d3979df1becc33de99535bac5a4dc80b3477317
Tamal <ckbtamaldipnew@gmail.com>
2026-02-08 18:27:17 +0600

    add shadcn to the frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,5 +1,9 @@
+import { Button } from "@/components/ui/button"
+
 export default function Home() {
   return (
-    <h1 className="text-2xl font-bold text-red-500">Hello world!</h1>
+    <div>
+      <Button>Click Me</Button>
+    </div>
   );
 }
