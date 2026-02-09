60e09c731ea1b26abe95d1328119573efa979ff4
Tamal <ckbtamaldipnew@gmail.com>
2026-02-09 20:19:15 +0600

    Add generate component strucutre

--- frontend/src/components/Generate.tsx
+++ frontend/src/components/Generate.tsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Input } from './ui/input'
+import { Button } from './ui/button'
 
 const Generate = () => {
   return (
-    <div className='h-full w-full flex flex-col items-center justify-center'>Generate</div>
+    <div className='h-full w-full flex flex-col items-center justify-center gap-3'>
+        <div className='flex flex-col'>
+            <span className='mb-2'>Provide your master key</span>
+            <Input/>
+        </div>      
+        <Button>
+            Generate .vault file 
+        </Button>      
+        <Button>
+            Generate Pass Key
+        </Button>           
+        <Button>
+            Done
+        </Button>           
+    </div>
   )
 }
 
