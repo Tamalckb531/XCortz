c3961c090eca53fa8aa5301d0b9ea4557878a9b3
Tamal <ckbtamaldipnew@gmail.com>
2026-02-09 20:20:52 +0600

    add upload component structure

--- frontend/src/components/Upload.tsx
+++ frontend/src/components/Upload.tsx
@@ -1,6 +1,23 @@
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+
 const Upload = () => {
   return (
-    <div className='h-full w-full flex flex-col items-center justify-center'>Upload</div>
+    <div className='h-full w-full flex flex-col items-center justify-center gap-3'>
+        <div className='flex flex-col'>
+            <span className='mb-2'>Provide your master key</span>
+            <Input/>
+        </div>      
+        <Button>
+            Upload your .vault file 
+        </Button>      
+        <Button>
+            Upload your Pass Key
+        </Button>           
+        <Button>
+            Go to Dashboard
+        </Button>           
+    </div>
   )
 }
 
