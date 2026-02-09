ad98cce160c8094a4f8c83cd2a39b0c46ac97ef9
Tamal <ckbtamaldipnew@gmail.com>
2026-02-09 19:50:25 +0600

    fix

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import MainPage from "@/components/MainPage";
 
 export default function Home() {
   return (
-    <div className=" bg-black text-white w-screen h-screen">
+    <div className="bg-black text-white w-screen h-screen">
       <MainPage/>
     </div>
   );
