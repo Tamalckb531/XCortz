8f3cb5e5398f85bfca791c3b320a8428c5cc3894
Tamal <ckbtamaldipnew@gmail.com>
2026-02-09 22:00:30 +0600

    Setup Dashboard

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -1,8 +1,8 @@
-import React from 'react'
+import Dashboard from '@/components/Dashboard'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div className='w-full h-full'><Dashboard/></div>
   )
 }
 
---
+++ frontend/src/components/Dashboard.tsx
@@ -0,0 +1,7 @@
+const Dashboard = () => {
+  return (
+    <div className=" w-full h-full flex flex-col items-center justify-center">Dashboard</div>
+  )
+}
+
+export default Dashboard
