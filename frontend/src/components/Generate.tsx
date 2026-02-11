3fb85f1f56a90fa42f34d848c0bc25feddfd421c
Tamal <ckbtamaldipnew@gmail.com>
2026-02-11 22:47:37 +0600

    generate download file issue resolved

--- frontend/src/components/Generate.tsx
+++ frontend/src/components/Generate.tsx
@@ -39,6 +39,7 @@ const Generate = () => {
         const url = URL.createObjectURL(blob);
         const link = document.createElement('a');
         link.href = url;
+        link.download = filename; 
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
