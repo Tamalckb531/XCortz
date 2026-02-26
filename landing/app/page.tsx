abec852a1849c20f45c80814cabad1be6c1d2dee
Tamal <ckbtamaldipnew@gmail.com>
2026-02-26 23:47:29 +0600

    add hero section gradient

--- landing/app/page.tsx
+++ landing/app/page.tsx
@@ -1,9 +1,18 @@
-import { Button } from "@/components/ui/button";
+import HeroSection from "@/components/HeroSection";
 
 export default function Home() {
   return (
-    <div className="">
-      <Button>Hello</Button>
+  <div className="min-h-screen w-full relative bg-black">
+    <div
+      className="absolute inset-0 z-0"
+      style={{
+        background: "radial-gradient(ellipse 80% 60% at 50% 0%, rgba(16, 185, 129, 0.25), transparent 70%), #000000",
+      }}
+    />
+  
+    <div className="relative z-10 p-5">
+      <HeroSection />
     </div>
+  </div>
   );
 }
