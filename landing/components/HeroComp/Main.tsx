2469ef3929e0f0babd615c4024b29e0551dd17d0
Tamal <ckbtamaldipnew@gmail.com>
2026-02-28 20:47:15 +0600

    add text in Main

--- landing/components/HeroComp/Main.tsx
+++ landing/components/HeroComp/Main.tsx
@@ -3,9 +3,11 @@ import CTA from '../common/CTA'
 
 const Main = () => {
   return (
-    <div className='flex flex-col gap-5 items-start justify-around'>
-      <h2></h2>
-      <p></p>
+    <div className='flex flex-col gap-5 items-start justify-around mt-16'>
+      <h2 className=' text-2xl'>Only Trust Thyself</h2>
+      <p>Shakespeare never said that. But we think he was onto something.
+        Your passwords live on your machine. No cloud. No network. No one else. Just you, your vault, and a master key only you know. XCortz keeps it that way always :)
+      </p>
       <CTA/>
     </div>
   )
